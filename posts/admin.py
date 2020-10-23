6cf07937310a0c28b10841f84d7cd9b8beb5dd77
Shaswat Agrawal <shaswatagrawl01@gmail.com>
2020-10-23 19:56:31 +0530

    Added fields to Post models such as creation date and all

--- posts/admin.py
+++ posts/admin.py
@@ -3,4 +3,7 @@ from .models import Post , Post_reply
 # Register your models here.
 
 admin.site.register(Post)
+
+
+
 admin.site.register(Post_reply)
--- posts/models.py
+++ posts/models.py
@@ -1,14 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils import timezone
+
+#To Do:-
+# add togs or category models
 
 class Post( models.Model):
+    STATUS_CHOICES = (
+        ('created' , 'Created')
+    )
 
-    title = models.CharField( max_length = 225 )
-    title_tag = models.CharField(max_length=225)
+    title = models.CharField( max_length = 250 )
+    title_tag = models.CharField(max_length=250)
     author = models.ForeignKey( User , on_delete=models.CASCADE )
     body = models.TextField()
     #publish = models.DateTimeField( auto_now_add = True , name='date published')
+    created = models.DateTimeField( default= timezone.now )
+
+    slug = models.SlugField( max_length= 300 , unique_for_date='created' )
+
+    class Meta:
+        ordering = ('-created',)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
