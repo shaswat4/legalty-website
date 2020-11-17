b705505a8237f0bcb72def17036e76be28874119
Shaswat Agrawal <shaswatagrawl01@gmail.com>
2020-11-17 19:51:45 +0530

    added fields in profile

--- users/models.py
+++ users/models.py
@@ -7,6 +7,12 @@ from django.utils import timezone
 class LawyerProfile( models.Model ):
     user = models.OneToOneField( User , null=True , on_delete = models.CASCADE )
     about = models.TextField()
+    speciality = models.CharField(max_length=100 , default = "" )
+    court = models.CharField(max_length=100 , default = "" )
+    fees = models.CharField(max_length=100 , default = "")
+    experience = models.IntegerField(max_length=100 , default = 1)
+    area = models.CharField(max_length=100 , default = "")
+
 
 
 class Review ( models.Model):
